6f4671d2e07393c0195ea9b13b6cb7164a4f2a91
Jérémy Jégou <jejeavo@gmail.com>
2014-12-23 18:06:08 +0100

    Add code style interpreter Change answers process
    Signed-off-by: Jérémy Jégou <jejeavo@gmail.com>

--- Resources/public/js/question.js
+++ Resources/public/js/question.js
@@ -1,23 +1,38 @@
 (function($) {
-    $.initAnswer = function () {
+    var addClose = function() {
+        return '<button type="button" class="close" data-dismiss="alert">' +
+        '<span aria-hidden="true">&times;</span>' +
+        '<span class="sr-only">Close</span>' +
+        '</button>';
+    };
+
+    var bindShow = function() {
+        $('#answers_form').find('button.close').click(function() {
+            $('#add-answer').show();
+        });
+    };
+
+    $.initAnswer = function (answersMax, strict) {
         $('#add-answer').click(function() {
             var answerCount = $('#answers_form button').length;
+
+            if (answerCount + 1 == answersMax && strict == 1) {
+                $('#add-answer').hide();
+            }
+
             var answerList = $('#question_form');
             var newWidget = answerList.attr('data-prototype');
             newWidget = newWidget.replace(/__name__/g, answerCount);
 
             var newAnswer = $('<div class="alert alert-danger" role="alert"></div>').html(addClose() + newWidget);
+            bindShow();
+
             newAnswer.appendTo($('#answers_form'));
 
             return false;
         });
 
-        var addClose = function() {
-            return '<button type="button" class="close" data-dismiss="alert">' +
-            '<span aria-hidden="true">&times;</span>' +
-            '<span class="sr-only">Close</span>' +
-            '</button>';
-        };
+        bindShow();
 
         var checkbox = $('#answers_form input[type="checkbox"]');
 
